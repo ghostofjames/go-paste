ad7bcfee03f4febde757aa3c3d0498d98c66f0cc
James Bloxham <11bloxhamj@gmail.com>
2022-10-24 14:47:20 +0100

    added read file functionality added index page

--- main.go
+++ main.go
@@ -31,10 +31,13 @@ func uploadHandler(w http.ResponseWriter, req *http.Request) {
 	}
 	defer file.Close()
 
+	log.Println(file)
+
 	log.Println(handler)
 
 	// Create new file
 	filename := fmt.Sprint("file-", next_id())
+
 	dst, err := os.Create(filepath.Join(folder, filename))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -43,12 +46,39 @@ func uploadHandler(w http.ResponseWriter, req *http.Request) {
 	defer dst.Close()
 
 	// Write file
-	if _, err := io.Copy(dst, file); err != nil {
+	_, err = io.Copy(dst, file)
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	io.WriteString(w, filename)
+	// Return file name
+	io.WriteString(w, filename+"\n")
+}
+
+func readHandler(w http.ResponseWriter, req *http.Request) {
+	filename := req.URL.Path[1:]
+	path := filepath.Join(folder, filename)
+
+	dat, err := os.ReadFile(path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(dat)
+
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	files, err := os.ReadDir(filepath.Join(folder))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		io.WriteString(w, f.Name()+"\n")
+	}
 }
 
 func main() {
@@ -61,8 +91,10 @@ func main() {
 		log.Fatal(err)
 	}
 
+	http.HandleFunc("/index", indexHandler)
 	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/", readHandler)
 
-	log.Println("Listing for requests at http://localhost:8000/hello")
+	log.Println("Listing for requests at http://localhost:8000/")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
